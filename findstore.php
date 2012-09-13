f2cac082c900b3286b5d15b801b82cdbff6aacf8
vlab81 <vibrancylab@gmail.com>
2012-09-13 09:20:53 +0800

    files

---
+++ findstore.php
@@ -0,0 +1,89 @@
+<?php
+include 'head.php';
+?>
+			<div id="breadcrumb">Home / Find A Store
+            	  <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-findstore.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            
+      <div class="clear"></div>
+              
+              <div class="storeWrapper">
+                <div class="storeAddress">
+                <h2>Jurong Point</h2>
+                <p>1 Jurong West Central 2</p>
+                <p>Jurong Point Shopping Center, #03-66</p>
+<p>Singapore 648886</p>
+                <p class="divider">Tel: 6790 0332</p>
+                <p class="divider">Open: Mon - Sun 11am to 9.30pm</p>
+                </div><!-- end of storeAddress -->
+                <div class="storeMap"><div id="map2"></div></div>
+                </div><!-- end of storeWrapper -->
+      <div class="clear"></div>
+                
+              <div class="storeWrapper">
+                <div class="storeAddress">
+                <h2>Bugis Junction</h2>
+                <p>200 Victoria Street</p>
+                <p> Bugis Junction, #02-11</p>
+<p>Singapore 188021</p>
+                <p class="divider">Tel: 6338 2773</p>
+                <p class="divider">Open: Mon - Sun 10.30am to 9.30pm</p>
+                </div><!-- end of storeAddress -->
+                <div class="storeMap"><div id="map3"></div></div>
+                </div><!-- end of storeWrapper -->
+      <div class="clear"></div>
+                
+              <div class="storeWrapper">
+                <div class="storeAddress">
+                <h2>Vivo City</h2>
+                <p>1 Maritime Walk</p>
+                <p>Vivo City, #02-95 </p>
+<p>Singapore 099253</p>
+                <p class="divider">Tel: 6272 2880</p>
+                <p class="divider">Open: Mon - Fri 10.30am to 9.30pm<br />
+                  Sat - Sun  10.30am to- 10pm</p>
+                </div><!-- end of storeAddress -->
+                <div class="storeMap"><div id="map4"></div></div>
+                </div><!-- end of storeWrapper -->
+      <div class="clear"></div>
+              
+              <div class="storeWrapper">
+                <div class="storeAddress">
+                <h2>Queensway</h2>
+                <p>1 Queensway</p>
+                <p>Queensway Shopping Center, #01-23</p>
+<p class="divider">Tel: 6476 2183</p>
+                <p class="divider">Open: Mon - Sun 11am to 9.30pm</p>
+                </div><!-- end of storeAddress -->
+                <div class="storeMap"><div id="map5"></div></div>
+                </div><!-- end of storeWrapper -->
+        <div class="clear"></div>
+
+            <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
