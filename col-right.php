9381080c8e0075a00066555882a39f8a119ed304
vlab81 <vibrancylab@gmail.com>
2012-09-24 14:47:05 +0800

    col right

---
+++ col-right.php
@@ -0,0 +1,12 @@
+<div class="box">
+    <h3>Upcoming Events</h3>
+    </div>
+<div class="box">
+    <p><img src="images/col-right-photoshoot_2.jpg" /></p>
+    <p>&nbsp;</p>
+    <h3>Urban Aesthetic Collection</h3>
+    <p>EverlastSG latest <a href="catalog/2012.html">E-Catalog</a></p>
+</div>
+<div class="box">
+	<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like-box href="http://www.facebook.com/everlastsg" width="200" colorscheme="dark" show_faces="true" border_color="" stream="false" header="true"></fb:like-box>
+</div>
