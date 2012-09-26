afbcb05c7efb12e24484cc5ede678bc2a5d76f2c
vlab81 <vibrancylab@gmail.com>
2012-09-26 15:20:19 +0800

    boxing page

---
+++ equip-boxing.php
@@ -0,0 +1,52 @@
+<?php
+include 'head.php';
+?>
+			<div id="breadcrumb">Home / Equipments / Boxing <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-equip.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+            	<li><a href="equip/protex3-heavy-bag-gloves.html"><img src="images/equip/protex3-heavy-bag-gloves_s.jpg" /> Protex3 Heavy Bag Gloves</a></li>
+            	<li><a href="equip/black-pu-training-gloves.html"><img src="images/equip/black-pu-training-gloves.jpg" /> Black PU Training Gloves</a></li>
+            	<li><a href="equip/red-pu-training-gloves.html"><img src="images/equip/red-pu-training-gloves.jpg" /> Red PU Training Gloves</a></li>
+                <li><a href="equip/sparkling-pro-style-gloves.html"><img src="images/equip/sparkling-pro-style-gloves.jpg" /> Sparkling Pro Style Gloves</a></li>
+                <li><a href="equip/white-pu-training-gloves.html"><img src="images/equip/white-pu-training-gloves.jpg" /> White PU Training Gloves</a></li>
+                <li><a href="equip/protex-training-gloves.html"><img src="images/equip/protex-training-gloves.jpg" /> Protex Training Gloves</a></li>
+                <li><a href="equip/black-leather-head-gear.html"><img src="images/equip/black-leather-head-gear.jpg" /> Black Leather Head Gear</a></li>
+                <li><a href="equip/red-leather-head-gear.html"><img src="images/equip/red-leather-head-gear.jpg" /> Red Leather Head Gear</a></li>
+                <li><a href="equip/protex2-training-gloves.html"><img src="images/equip/protex2-training-gloves.jpg" /> Protex<sup>2</sup> Training Gloves</a></li>
+                <li><a href="equip/protex2-evergel-training-gloves.html"><img src="images/equip/protex2-evergel-training-gloves.jpg" /> Protex<sup>2</sup> Evergel Training Gloves</a></li>
+                <li><a href="equip/wristwrap-heavy-bag-gloves.html"><img src="images/equip/wristwrap-heavy-bag-gloves.jpg" /> Wristwrap Heavy Bag Gloves</a></li>
+                <li><a href="equip/evergel-wristwrap-heavy-gloves.html"><img src="images/equip/evergel-wristwrap-heavy-gloves.jpg" /> Evergel<sup>TM</sup> Wristwrap Heavy Bag Gloves</a></li>
+                <li><a href="equip/mantis-punch-mitts.html"><img src="images/equip/mantis-punch-mitts.jpg" /> Mantis Punch Mitts</a></li>
+                <li><a href="equip/gym-bag-gloves.html"><img src="images/equip/gym-bag-gloves.jpg" /> Gym Bag Gloves</a></li>
+            </ul><!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+           <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
