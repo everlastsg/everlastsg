aeca8619184a80b19db448ec7f498572901a5678
vlab81 <vibrancylab@gmail.com>
2012-09-26 09:46:01 +0800

    update of e-catalog

--- col-right.php
+++ col-right.php
@@ -5,7 +5,7 @@
     <p><img src="images/col-right-photoshoot_2.jpg" /></p>
     <p>&nbsp;</p>
     <h3>Urban Aesthetic Collection</h3>
-    <p>EverlastSG latest <a href="catalog/2012.html">E-Catalog</a></p>
+    <p>EverlastSG latest <a href="catalog.html">E-Catalog</a></p>
 </div>
 <div class="box">
 	<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like-box href="http://www.facebook.com/everlastsg" width="200" colorscheme="dark" show_faces="true" border_color="" stream="false" header="true"></fb:like-box>
