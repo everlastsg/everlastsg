193dc192c60818bc1e9967914090914c85ba677e
vlab81 <vibrancylab@gmail.com>
2012-09-27 09:32:09 +0800

    accessories

---
+++ equip-accessories.php
@@ -0,0 +1,50 @@
+<?php
+include 'head.php';
+?>
+			<div id="breadcrumb">Home / Equipments / Accessories <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-equip.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+           		<li><a href="equip/108-cotton-hand-wraps.html"><img src="images/equip/108-cotton-hand-wraps_s.jpg" />108" Cotton Handwraps</a></li>
+                <li><a href="equip/mma-handwraps.html"><img src="images/equip/mma-handwraps_s.jpg" />MMA 100" Handwraps</a></li>
+            	<li><a href="equip/180-handwraps.html"><img src="images/equip/180-handwraps_s.jpg" />180" Handwraps</a></li>
+				<li><a href="equip/glove-bag.html"><img src="images/equip/glove-bag_s.jpg" /> Glove Bag</a></li>
+           		<li><a href="equip/leather-jump-rope.html"><img src="images/equip/leather-jump-rope_s.jpg" /> Leather Jump Rope</a></li>
+            	<li><a href="equip/classic-leather-speed-bag.html"><img src="images/equip/classic-leather-speed-bag.jpg" /> Classic Leather Speed Bag</a></li>
+                <li><a href="equip/leather-speed-bag.html"><img src="images/equip/leather-speed-bag.jpg" /> Leather Speed Bag</a></li>
+                <li><a href="equip/single-guard-mouthguard.html"><img src="images/equip/single-guard-mouthguard.jpg" /> Single Guard Mouthguard</a></li>
+                <li><a href="equip/double-guard-mouthguard.html"><img src="images/equip/double-guard-mouthguard.jpg" /> Double Guard Mouthguard</a></li>
+                <li><a href="equip/120-junior-mexican-handwraps.html"><img src="images/equip/120-junior-mexican-handwraps.jpg" /> 120" Junior Mexican Handwraps</a></li>
+                <li><a href="equip/108-cotton-handwraps.html"><img src="images/equip/108-cotton-handwraps.jpg" /> 108" Cotton Handwraps</a></li>
+                <li><a href="equip/heavy-bag-chain-assembly.html"><img src="images/equip/heavy-bag-chain-assembly.jpg" /> Heavy Bag Chain Assembly</a></li>
+            </ul><!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+            <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
