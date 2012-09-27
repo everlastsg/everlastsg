a2a27aff7c9ba3610aadedc08edb2b665396ccd6
vlab81 <vibrancylab@gmail.com>
2012-09-27 12:16:09 +0800

    mma

---
+++ equip-mma.php
@@ -0,0 +1,47 @@
+<?php
+include 'head.php';
+?>
+			<div id="breadcrumb">Home / Equipments / MMA <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-equip.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+            	<li><a href="equip/mma-kickboxing-gloves.html"><img src="images/equip/mma-kickboxing-gloves_s.jpg" /> MMA Kickboxing Gloves</a></li>
+          	    <li><a href="equip/muay-thai-gloves.html"><img src="images/equip/muay-thai-gloves_s.jpg" /> Muay Thai Gloves</a></li>
+            	<li><a href="equip/mma-grappling-training-gloves.html"><img src="images/equip/mma-grappling-training-gloves.jpg" /> Grappling Training Gloves</a></li>
+            	<li><a href="equip/mma-grappling-gloves.html"><img src="images/equip/mma-grappling-gloves.jpg" /> Grappling Gloves</a></li>
+            	<li><a href="equip/mma-sparring-gloves.html"><img src="images/equip/mma-sparring-gloves.jpg" /> MMA Sparring Gloves</a></li>
+                <li><a href="equip/mma-prostyle-grappling-gloves.html"><img src="images/equip/mma-prostyle-grappling-gloves.jpg" /> Pro Style Grappling Gloves</a></li>
+                <li><a href="equip/mma-blocker.html"><img src="images/equip/mma-blocker.jpg" /> MMA Blocker</a></li>
+                <li><a href="equip/mma-punchkick-paddles.html"><img src="images/equip/mma-punchkick-paddles.jpg" /> MMA Punch + Kick Paddles</a></li>
+                <li><a href="equip/mma-thai-pads.html"><img src="images/equip/mma-thai-pads.jpg" /> MMA Thai Pads</a></li>
+            </ul><!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+            <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
