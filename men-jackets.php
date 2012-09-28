7aa70a4f9e10e969155ca6a5ba9220d25b0dcae9
vlab81 <vibrancylab@gmail.com>
2012-09-28 13:50:21 +0800

    initial

---
+++ men-jackets.php
@@ -0,0 +1,45 @@
+<?php
+include 'head.php';
+?>
+            <div id="breadcrumb">Home / Men /
+              Jackets
+                <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-men-jackets.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+            <li><a href="men/4630137LMI2.html"><img src="images/men/4630137LMI2.jpg" /> Everlast Jacket</a></li>
+            <li><a href="men/4630132LMI1.html"><img src="images/men/4630132LMI1.jpg" /> Everlast Jacket</a></li>
+            <li><a href="men/4630125LMH4.html"><img src="images/men/4630125LMH4.jpg" /> Everlast Jacket</a></li>
+            <li><a href="men/4630120LMH3.html"><img src="images/men/4630120LMH3.jpg" /> Everlast Jacket</a></li>
+            	
+            </ul><!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+           <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
