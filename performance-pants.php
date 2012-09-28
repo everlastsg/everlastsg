8e9aa6774810d31e83745d3df46960f5c5f8e076
vlab81 <vibrancylab@gmail.com>
2012-09-28 16:45:38 +0800

    initial

---
+++ performance-pants.php
@@ -0,0 +1,45 @@
+<?php
+include 'head.php';
+?>
+            	<div id="breadcrumb">Home / Performance / Pants <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-performance-pants.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+            	<li><a href="performance/5230015LMI1.html"><img src="images/performance/5230015LMI1.jpg" /> Men's Pants</a></li>
+            	<li><a href="performance/5230014LMI1.html"><img src="images/performance/5230014LMI1.jpg" /> Men's Pants</a></li>
+            	<li><a href="performance/5630034LWI1.html"><img src="images/performance/5630034LWI1.jpg" /> Men's Pants</a></li>
+            	<li><a href="performance/5730095LMH3.html"><img src="images/performance/5730095LMH3.jpg" /> Men's Pants</a></li>
+                <li><a href="performance/5430009LWH3.html"><img src="images/performance/5430009LWH3.jpg" /> Women's Pants</a></li>
+            	<li><a href="performance/5430006LWH3.html"><img src="images/performance/5430006LWH3.jpg" /> Women's Pants</a></li>
+            	            	
+            </ul><!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+            <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
