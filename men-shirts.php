5ad549530402af0b8d74dbc037e55f3bc9a2e2f3
vlab81 <vibrancylab@gmail.com>
2012-09-28 11:42:11 +0800

    shirt commit

---
+++ men-shirts.php
@@ -0,0 +1,41 @@
+<?php
+include 'head.php';
+?>
+            <div id="breadcrumb">Home / Men / Shirts
+              <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-men-shirts.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+            	<li><a href="men/4330003LMH3.html"><img src="images/men/4330003LMH3.jpg" /> Everlast Shirt</a></li>
+            	
+            </ul><!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+           <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
