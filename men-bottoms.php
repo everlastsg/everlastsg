5a7f1e725cb91651973a56dc0d6f63128ea376ef
vlab81 <vibrancylab@gmail.com>
2012-10-12 10:38:31 +0800

    initial commit

---
+++ men-bottoms.php
@@ -0,0 +1,47 @@
+<?php
+include 'head.php';
+?>
+            <div id="breadcrumb">Home / Men / Bottoms <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-men-bottoms.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+            	<li><a href="men/5430017LMI3.html"><img src="images/men/5430017LMI3.jpg"/> Bermuda Shorts</a></li>
+           		<li><a href="men/5630036LMI1.html"><img src="images/men/5630036LMI1.jpg"/> Pants</a></li>
+                <li><a href="men/5430015LMI1.html"><img src="images/men/5430015LMI1.jpg"/> Bermuda Shorts</a></li>
+                <li><a href="men/5430014LMI1.html"><img src="images/men/5430014LMI1.jpg"/> Bermuda Shorts</a></li>
+                <li><a href="men/126578.html"><img src="images/men/126578.jpg"/> Bermuda Shorts</a></li>
+                <li><a href="men/126589.html"><img src="images/men/126589.jpg"/> Bermuda Shorts</a></li>
+                <li><a href="men/5430007LMH3.html"><img src="images/men/5430007LMH3.jpg"/> Bermuda Foldup</a></li>
+                <li><a href="men/5430008LMH3.html"><img src="images/men/5430008LMH3.jpg"/> Bermuda Foldup</a></li>
+                               
+             </ul><!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+           <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
