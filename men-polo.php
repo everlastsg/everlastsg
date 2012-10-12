17bfc60cde3d411b0143e608d62f31fd125e8ae7
vlab81 <vibrancylab@gmail.com>
2012-10-12 10:41:26 +0800

    initial commit

---
+++ men-polo.php
@@ -0,0 +1,51 @@
+<?php
+include 'head.php';
+?>
+            	<div id="breadcrumb">Home / Men / Polo Tees <hr /></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            <div class="main_view">
+                <div class="window">
+                    <div class="image_reel">
+                        <img src="images/img-men.jpg" alt="" width="540" height="260" />
+                    </div>
+                </div>
+            </div><!-- end of main_view -->
+            <div id="paging_container">
+            <ul class="prodlist">
+           		<li><a href="men/4230210LMI2.html"><img src="images/men/4230210LMI2.jpg"/> M Everlast Polo</a></li>
+           		<li><a href="men/4230212LMI3.html"><img src="images/men/4230212LMI3.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230209LMI1.html"><img src="images/men/4230209LMI1.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230207LWI1.html"><img src="images/men/4230207LWI1.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230203LMI1.html"><img src="images/men/4230203LMI1.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230205LMI1.html"><img src="images/men/4230205LMI1.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230202LMI1.html"><img src="images/men/4230202LMI1.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230201LMI1.html"><img src="images/men/4230201LMI1.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230193LMH4.html"><img src="images/men/4230193LMH4.jpg"/> M Everlast Polo</a></li>
+            	<li><a href="men/4230195LMH4.html"><img src="images/men/4230195LMH4.jpg"/> M Everlast Polo</a></li>
+               	<li><a href="men/4230191LMH3.html"><img src="images/men/4230191LMH3.jpg"/> M Everlast Polo</a></li>
+           		<li><a href="men/4230187LMH3.html"><img src="images/men/4230187LMH3.jpg"/> M Everlast Polo</a></li>
+            </ul>
+            <!-- end of prodlist -->
+            <div class="page_navigation"></div>
+            </div><!-- end of paging_container -->
+            <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
