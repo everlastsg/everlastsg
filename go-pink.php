010afdb435867188d830ee4e17e6a21cd2fcc152
vlab81 <vibrancylab@gmail.com>
2012-10-15 09:15:08 +0800

    initial commit

---
+++ go-pink.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Everlast Singapore | www.everlastsg.com</title>
+<meta name="description" content="" />
+<meta name="keywords" content="everlast, everlast singapore, boxing, martial arts, boxing equipments, everlastsg, boxing gloves" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> 
+
+<link href="css/global.css" rel="stylesheet" type="text/css" />
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+<link href="css/jcarousel-skin.css" rel="stylesheet" type="text/css" />
+<link href="css/nivo-slider.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" media="all" href="unleash/css/jquery.fancybox.css" />
+<style>
+body{background:white url(../images/pink-strip.jpg) 0 20px no-repeat;}
+.tab_container {background:#c12d5b;}
+html ul.tabs li.active, html ul.tabs li.active a, html ul.tabs li.active a:hover {background:#c12d5b; color:#fff !important;}
+#slider .nivo-controlNav a{background:url(../images/bg_slide-pink.png) no-repeat;}
+#breadcrumb{background:url(images/go-pink-header.png) no-repeat;height:43px;}
+
+</style>
+<script language="javascript" type="text/javascript" src="unleash/js/jquery-1.8.0.min.js"></script>
+<script type="text/javascript" src="js/home.js"></script>
+<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+<script type="text/javascript" src="js/accordion.js"></script>
+<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+
+
+        <script language="javascript" type="text/javascript" src="unleash/js/jquery.easing.1.3.js"></script>
+        <script language="javascript" type="text/javascript" src="unleash/js/jquery.fancybox-pink.js"></script>
+        <script language="javascript" type="text/javascript" src="unleash/js/catalog-controller.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+    $('#slider').nivoSlider({
+        effect:'fade' //Specify sets like: 'fold,fade,sliceDown'
+    });
+	$('.jcarousel-next,.jcarousel-next-horizontal,.jcarousel-next-disabled,.jcarousel-next-disabled-horizontal,.jcarousel-prev,.jcarousel-prev-horizontal,.jcarousel-prev-disabled,.jcarousel-prev-disabled-horizontal').hide();
+	
+});
+
+
+</script>
+
+<!--[if lte IE 6]>
+<script type="text/javascript" src="js/supersleight.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+<div id="wrapper-home">
+	<div id="wrap-content">
+    	<div id="col-left">
+        	<a href="index.php"><img src="images/logo.png" width="124" height="82" /></a>
+            <ul class="menu">
+                <li>
+                    <a href="#" class="navi-men">Men</a>
+                    <ul class="acitem">
+                        <li><a href="men-polo.php">Polo Tees</a></li>
+                        <li><a href="men-graphic.php">Graphic Tees</a></li>
+                        <li><a href="men-jackets.php">Jackets</a></li>
+                        <li><a href="men-boxing.php">Boxing Shorts</a></li>
+                        <li><a href="men-bottoms.php">Bottoms</a></li>
+                        <li><a href="men-shirts.php">Shirts</a></li>
+                     </ul>
+                </li>
+                <li>
+                  <a href="#" class="navi-women">Women</a>
+                    <ul class="acitem">
+                        <li><a href="women-polo.php">Polo Tees</a></li>
+                         <li><a href="women-graphic.php">Graphic Tees</a></li>
+                          <li><a href="women-bottoms.php">Bottoms</a></li>
+                          <li><a href="women-jackets.php">Jackets</a></li>
+                                                 
+                    </ul>
+                </li>
+                <li>
+                  <a href="#" class="navi-performance">Performance</a>
+                    <ul class="acitem">
+                        <li><a href="performance-tops.php">Tops</a></li>
+                         <li><a href="performance-pants.php">Pants</a></li>
+                                                 
+                    </ul>
+                </li>
+                 <li>
+                  <a href="#" class="navi-muhdali">Muhd Ali</a>
+                    <ul class="acitem">
+                        <li><a href="muhdali-tops.php">Tops</a></li>
+                         <li><a href="muhdali-pants.php">Pants</a></li>
+                     </ul>
+                </li>
+                <li>
+                  <a href="#" class="navi-equip">Equipments</a>
+                    <ul class="acitem">
+                        <li><a href="equip-boxing.php">Boxing</a></li>
+                        <li><a href="equip-mma.php">MMA</a></li>
+                        <li><a href="equip-accessories.php">Accessories</a></li>
+                        
+                    </ul>
+          		</li>
+                 <li><a href="bags.php" class="navi-bags">Bags</a></li>
+                <li><a href="aboutus.php" class="navi-aboutus">About Us</a></li>
+                <li><a href="events.php" class="navi-events">Events</a></li>
+                <li><a href="findstore.php" class="navi-findstore">Find a store</a></li>
+                <li><a href="contactus.php" class="navi-contact">Contact Us</a></li>
+            </ul><!-- end of menu navigation -->
+            <div class="banner-box1"><hr /><a href="equip-boxing.php"><img src="images/banner-1.png" width="128" height="346" /></a></div><!-- end of banner 1 -->
+        </div><!-- end of col-left -->
+        <div id="col-main">
+        	<div id="header">
+                <ul id="socialmedia">
+                    <li><a href="http://www.facebook.com/everlastsg/"><img src="images/icon-facebook.png" width="16" height="16" /></a></li>
+                    <li><a href="http://www.twitter.com/everlastsg/"><img src="images/icon-twitter.png" width="16" height="16" /></a></li>
+                </ul><!-- end of socialmedia -->
+            	<div id="breadcrumb"></div><!-- end of breadcrumb -->
+            </div><!-- end of header -->
+            
+            <div id="slider" class="nivoSlider">
+            	
+                <img src="images/slide_p1.jpg" alt="" />
+                <a class="fancybox fancybox.iframe" href="pink-video.html"><img src="images/slide_p2.jpg" alt="" /><img src="images/slide_p2.jpg" alt="" /></a>
+           		<img src="images/slide_p3.jpg" alt="" />
+                <!--a href="#"><img src="images/slide_p4.jpg" alt="" /></a>
+                <a href="#"><img src="images/slide_p5.jpg" alt="" /></a-->
+            </div><!-- end of slider -->
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>Everlast has proudly supported the Breast Cancer Research Foundation® (BCRF) since 2006. By donating a portion from each sale of select Everlast pink products including the ProStyle Training Glove, 108" hand wraps, and Evergel hand wraps, we continue assisting BCRF in its mission to raise awareness and find a cure for breast cancer.</p>
+            <p>&nbsp;</p>
+
+<p>Throughout the month of October, many Everlast fighters will be wearing pink gloves to show their support of the cause. You can do your part by purchasing select pink products to help support BCRF. With your help, we can win the fight against breast cancer!</p>
+            <div id="wrap-tab">
+            <ul class="tabs">
+                <li><a href="#latest">support breast cancer research</a></li>
+                
+            </ul>
+            
+            <div class="tab_container">
+                <div id="latest" class="tab_content">
+                    <!--Content-->
+                      <ul id="first-carousel" class="jcarousel-skin-tango">
+                      <li><a href="equip/pink-pro-style-training-gloves.html"><img src="images/equip/pink-pro-style-training-gloves.jpg" width="170" height="180" alt="" /></a></li>
+                      <li><a href="equip/108-cotton-hand-wraps.html"><img src="images/equip/108-cotton-hand-wraps_s.jpg" width="170" height="180" alt="" /></a></li>
+           		
+                       
+                      </ul>
+                     
+                      	
+                </div>
+                
+            </div><!-- end of tab_container -->
+            </div><!-- end of wrap-tab -->
+            <?php include 'footer.php'; ?>
+        </div><!-- end of col-main -->
+        <div id="col-right">
+        	<?php include 'col-right.php'; ?>
+        </div><!-- end of col-right -->
+    </div><!-- end of wrap-content -->
+</div><!-- end of wrapper -->
+
+
+<!-- google analytics -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8891208-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
